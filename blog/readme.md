8749c2e8aaf51c3e173384df0efe5ff3d4f89a6a
kunxin-chor <chorkunxin@yahoo.com>
2020-08-21 05:16:16 +0000

    Modified bootstrap function

---
+++ blog/readme.md
@@ -0,0 +1,9 @@
+# Creating a MVC web app using NestJS Controller
+
+## Dependencies (besides NestJS)
+We are going to use Handlebar.
+
+```
+npm install --s hbs
+npm install --s path
+```
--- blog/src/main.ts
+++ blog/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // set up the view engine for the Express App
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  // end setup
+
+  // Must ALWAYS be the last line in the `bootstrap` function
   await app.listen(3000);
 }
 bootstrap();
